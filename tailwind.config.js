4807e480d1acd7fda7667a0731b5290ed01adb6e
Steven <dubfunkle@gmail.com>
2025-05-20 12:41:51 +0200

    new tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,19 @@ module.exports = {
       "./public/**/*.js"
     ],
     safelist: [
-      'bg-red-500',
+      'bg-orange-500',
+      'bg-orange-600',
+      'bg-orange-700',
+      'hover:bg-orange-600',
+      'hover:bg-orange-700',
+      'from-orange-500',
+      'to-orange-600',
+      'bg-gradient-to-br',
       'text-white',
       'p-4',
       'rounded-md',
-      'text-gray-800',
-      'bg-orange-500',
-      'hover:bg-orange-600'
-    ],
+      'text-gray-800'
+    ],    
     theme: {
       extend: {},
     },
